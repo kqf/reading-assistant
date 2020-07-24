37e17a9af3ddb3269fd67b03d75b75bdb1dcd4c6
Sasha <q.dev.null@gmail.com>
2020-07-24 10:53:02 +0200

    Separate the gunicorn and click endpoints

--- assistant/main.py
+++ assistant/main.py
@@ -2,31 +2,33 @@ import click
 
 from app import create_app, db
 from app.models import User
-from app.settings import SQLALCHEMY_DATABASE_URI
 from assistant.find import find as bfind
 
 
-@click.group()
-def main():
-    pass
-
-
-@main.command()
-@click.option("--infile", type=str, default="text.txt")
-@click.option("--dictionary", type=str, default="google-10000-english.txt")
-def find(infile, dictionary):
-    bfind(infile, dictionary)
-
-
-@main.command()
-def serve():
-    print("Database path", SQLALCHEMY_DATABASE_URI)
+def main(*args, **kwargs):
     app = create_app()
     with app.app_context():
         db.create_all()
         if User.query.filter_by(username='john').first() is None:
             User.register('john', 'cat')
     app.run()
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.option("--infile", type=str, default="text.txt")
+@click.option("--dictionary", type=str, default="google-10000-english.txt")
+def find(infile, dictionary):
+    bfind(infile, dictionary)
+
+
+@cli.command()
+def serve():
+    main()
 
 
 if __name__ == '__main__':
