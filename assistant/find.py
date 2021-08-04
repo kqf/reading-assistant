abe1d79e9b64adb3bb42f18a416f77f94520c273
Sasha <q.dev.null@gmail.com>
2021-08-04 06:29:20 +0200

    Fix the find function not to use the io operations

--- assistant/find.py
+++ assistant/find.py
@@ -20,7 +20,7 @@ def valid(token):
     return token.is_alpha and not token.is_stop
 
 
-def find(infile, dictionary):
+def find(infile, dictionary, only_nouns=True):
     nlp = spacy.load("en_core_web_sm")
     with open(infile) as f:
         doc = nlp(f.read())
@@ -29,8 +29,10 @@ def find(infile, dictionary):
     spacy.tokens.Span.set_extension("oov", getter=is_oov)
     spacy.tokens.Token.set_extension("oov", getter=is_oov)
 
-    print("Noun phrases:")
-    print(list(set([span.text for span in doc.noun_chunks if span._.oov])))
+    nouns = list(set([span.text for span in doc.noun_chunks if span._.oov]))
 
-    print("Tokens:")
-    print(list([t.lemma_ for t in doc if t._.oov and valid(t)]))
+    if only_nouns:
+        return nouns
+
+    everything = list([t.lemma_ for t in doc if t._.oov and valid(t)])
+    return nouns + everything
